139567b29c6a814bc6fb129eb56d162cb01b8135
Edris <edrismoavenghafouri@gmail.com>
2024-06-14 18:53:00 +0330

    Gloa using and Injecting And Adding Factory method to the products

---
+++ src/Catalog.Api/GlobalUsing.cs
@@ -0,0 +1,3 @@
+global using Carter;
+global using Mapster;
+global using MediatR;
--- src/Catalog.Api/Models/Products.cs
+++ src/Catalog.Api/Models/Products.cs
@@ -2,6 +2,40 @@ namespace Catalog.Api.Models;
 
 public class Products
 {
+    private Products(string name, string description, string imageFile, decimal price, List<string> category)
+    {
+        Price = price;
+        Name = name;
+        Description = description;
+        ImageFile = imageFile;
+        Category = category;
+    }
+
+    public static Products CreateProducts(
+        string name,
+        string description, 
+        string imageFile,
+        decimal price,
+        List<string> category)
+    {
+        //TODO
+        //Custom Validatiom
+        
+        if (category == null) throw new ArgumentNullException(nameof(category));
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("Value cannot be null or whitespace.", nameof(name));
+        if (string.IsNullOrWhiteSpace(description))
+            throw new ArgumentException("Value cannot be null or whitespace.", nameof(description));
+        if (string.IsNullOrWhiteSpace(imageFile))
+            throw new ArgumentException("Value cannot be null or whitespace.", nameof(imageFile));
+        ArgumentOutOfRangeException.ThrowIfNegative(price);
+        
+        var product = new Products(name,description,imageFile, price,category);
+        
+        return product;
+    }
+
+
     public Guid Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
