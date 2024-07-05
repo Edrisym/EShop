7e7740012072fe2b9feeb4a4c1168638429bf614
Edris <edrismoavenghafouri@gmail.com>
2024-07-05 20:38:55 +0330

    adding redis and cacshing

--- src/Basket.Api/Program.cs
+++ src/Basket.Api/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 var assembly = typeof(Program).Assembly;
 builder.Services.AddCarter();
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
 builder.Services.AddMediatR(config =>
     {
         config.RegisterServicesFromAssembly(assembly);
@@ -24,6 +25,14 @@ builder.Services.AddMarten(opts =>
     opts.Connection(builder.Configuration.GetConnectionString("DefaultConnection")!);
     opts.Schema.For<ShoppingCart>().Identity(x => x.UserName);
 }).UseLightweightSessions();
+
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+    //options.InstanceName = "Basket";
+});
+
 
 var app = builder.Build();
 
