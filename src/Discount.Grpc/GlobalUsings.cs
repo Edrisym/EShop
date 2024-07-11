433d7af3ca1ebfe6cd1eea18230d3b310c82b013
Edris <edrismoavenghafouri@gmail.com>
2024-07-11 13:45:20 +0330

    Adding crud discount service

--- src/Discount.Grpc/GlobalUsings.cs
+++ src/Discount.Grpc/GlobalUsings.cs
@@ -2,3 +2,5 @@ global using Discount.Grpc.Services;
 global using Discount.Grpc.Models;
 global using Microsoft.EntityFrameworkCore;
 global using Discount.Grpc.Data;
+global using Grpc.Core;
+global using Mapster;
