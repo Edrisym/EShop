0a63d5058988bb775ec7a6009f6306a8812420c7
Edris <edrismoavenghafouri@gmail.com>
2024-07-15 12:06:04 +0330

    Modifying shopping cart field

--- src/Basket.Api/Models/ShoppingCart.cs
+++ src/Basket.Api/Models/ShoppingCart.cs
@@ -4,10 +4,14 @@ public class ShoppingCart
 {
     public string UserName { get; set; } = default!;
     public decimal TotalPrice => Items.Sum(x => x.Price * x.Quantity);
-    
-    public decimal TotalPriceWithDiscount => Items.Sum(x => x.Price * x.Quantity);
+    public decimal TotalDiscount => 
+        decimal.Subtract(TotalPrice, Items.Sum(x => x.Price));
+
+    public decimal TotalPriceWithDiscount =>
+        decimal.Subtract(TotalPrice, TotalDiscount);
+
     public List<ShoppingCartItem> Items { get; set; } = [];
-    // public IReadOnlyCollection<ShoppingCartItem> ShoppingCartItems => Items;
+    public IReadOnlyCollection<ShoppingCartItem> ShoppingCartItems => Items;
 
     public ShoppingCart(string userName)
     {
