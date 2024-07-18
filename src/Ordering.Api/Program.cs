2a571250bd64d2d1eff4d4604586225f286e4605
Edris <edrismoavenghafouri@gmail.com>
2024-07-18 17:16:05 +0330

    modifying Program pipelone

--- src/Ordering.Api/Program.cs
+++ src/Ordering.Api/Program.cs
@@ -4,9 +4,6 @@ using Ordering.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
-
 builder.Services
     .AddApplicationLayer()
     .AddInfrastructureLayer(builder.Configuration)
@@ -14,18 +11,5 @@ builder.Services
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-// if (app.Environment.IsDevelopment())
-// {
-// app.UseSwagger();
-// app.UseSwaggerUI();
-// }
-
-// app.UseHttpsRedirection();
 
 app.Run();
-
-// record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-// {
-// public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-// }
